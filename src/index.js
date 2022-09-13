2eb0e6500a90f959f03eb921348adade7bbbd4f8
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-13 14:41:39 +0600

    Color Input changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import createRoot from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/stylesheet/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
--- src/pages/home/Color.jsx
+++ src/pages/home/Color.jsx
@@ -8,26 +8,47 @@ const Color = (props) => {
         <h2 className="bg-amber-700">{props.error}</h2>
       </div>
 
-      <div className="d-flex justify-content-between">
-        <input
-          type="color"
-          //   ref={hex}
-          defaultValue={props.color1}
-          style={props.icolor1}
-          className="rounded text-center shadow-2xl w-28 h-12"
-          name="hex"
-          onChange={(e) => props.setColor1(e.target.value)}
-        />
+      <div className="s">
+        <div className="d-flex justify-content-between w-100 mb-2">
+          <div className="h5 w-25">Color 1</div>
+          <input
+            type="color"
+            //   ref={hex}
+            defaultValue={props.color1}
+            // Value={props.color1}
+            // style={props.icolor1}
+            className="rounded text-center w-25 p-0"
+            name="hex"
+            onChange={(e) => props.setColor1(e.target.value)}
+          />
+          <input
+            type="text"
+            className="w-25"
+            style={props.icolor1}
+            value={props.color1}
+            onChange={(e) => props.setColor1(e.target.value)}
+          />
+        </div>
 
-        <input
-          type="color"
-          // ref={hex2}
-          defaultValue={props.color2}
-          style={props.icolor2}
-          className="rounded text-center shadow-2xl w-28 h-12"
-          name="hex2"
-          onChange={(e) => props.setColor2(e.target.value)}
-        />
+        <div className="d-flex justify-content-between">
+          <div className="w-25 h5">Color 2</div>
+          <input
+            type="color"
+            // ref={hex2}
+            defaultValue={props.color2}
+            style={props.icolor2}
+            className="rounded w-25 text-center p-0"
+            name="hex2"
+            onChange={(e) => props.setColor2(e.target.value)}
+          />
+          <input
+            type="text"
+            className="w-25"
+            style={props.icolor2}
+            value={props.color2}
+            onChange={(e) => props.setColor2(e.target.value)}
+          />
+        </div>
       </div>
     </div>
   );
